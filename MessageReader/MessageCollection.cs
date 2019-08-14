ca9340a786445c81ef18a2ab2cad12d7175eddb3
David Kitchen <DevKitchen@outlook.com>
2019-08-14 23:06:47 +0100

    Add project files.

---
+++ MessageReader/MessageCollection.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace MessageReader
+{
+    public static class MessageCollection
+    {
+        [FunctionName("MessageCollection")]
+        public static void Run([QueueTrigger("messagedrop", Connection = "Storage")]string myQueueItem, ILogger log)
+        {
+            MessageRecord thisMessage = new MessageRecord(myQueueItem);
+
+            Task arduousTask = thisMessage.SaveRecord();
+
+            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+
+            arduousTask.Wait();
+        }
+    }
+}
---
+++ MessageReader/MessageRecord.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Threading.Tasks;
+
+namespace MessageReader
+{
+    public class MessageRecord : TableEntity
+    {
+        public string TheMessage { get; set; }
+
+        public MessageRecord() { }
+        public MessageRecord(string theMessage) : base()
+        {
+            this.PartitionKey = DateTime.Today.ToString("yyyyMMdd");
+            this.RowKey = string.Concat(DateTime.Now.ToString("HHmmss"), " : ", Guid.NewGuid());
+            TheMessage = theMessage;
+        }
+
+        public async Task SaveRecord()
+        {
+            string storageConnection = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            string tableName = Environment.GetEnvironmentVariable("TableName");
+
+            CloudStorageAccount thisAcc = CloudStorageAccount.Parse(storageConnection);
+            CloudTableClient thisClient = thisAcc.CreateCloudTableClient();
+            CloudTable thisTable = thisClient.GetTableReference(tableName);
+
+            await thisTable.CreateIfNotExistsAsync();
+
+            TableOperation thisOperation = TableOperation.InsertOrReplace(this);
+            await thisTable.ExecuteAsync(thisOperation);
+        }
+    }
+}
